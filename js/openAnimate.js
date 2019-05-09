a5297db6a28cc1ca47ee94f39265f1a09cda0109
Michael Redburn <redburnmichael@gmail.com>
2019-05-09 13:19:38 +1000

    Improvements to navigation dots

--- js/openAnimate.js
+++ js/openAnimate.js
@@ -4,21 +4,23 @@
     const shape = document.querySelector("#shape");
     const circle = document.querySelector("#circle");
     const dataY = document.querySelector(".scrollY");
-    const dot = document.querySelector(".dot");
+    const dotContain = document.querySelector("#dotContain");
+    const dot = document.querySelectorAll(".dot");
     const container = document.querySelector("#pageContain");
     const wHeight = document.body.offsetHeight;
     const numOfSlides = 5;
+    let currentSlide = 1;
     //title.classList.remove('hide');
     //title.classList.add('show');
-    let i = 0;
+    let p = 0;
     let txt = 'Title here'; /* The text */
     let speed = 100; /* The speed/duration of the effect in milliseconds */
     typeWriter();
 
     function typeWriter() {
-    if (i < txt.length) {
-        title.innerHTML += txt.charAt(i);
-        i++;
+    if (p < txt.length) {
+        title.innerHTML += txt.charAt(p);
+        p++;
         setTimeout(typeWriter, speed);
         console.log(wHeight);
     }
@@ -39,8 +41,8 @@
 
     function shapeMove(y){
         let rotationValue = y * 0.2;
-        let circleValueY = -y * 1;
-        let circleValueX = y * 1;
+        let circleValueY = -y * 0.5;
+        let circleValueX = y * 0.5;
         shape.style.transform = "rotate("+ rotationValue + "deg)";
         //shape.style.webkitTransform = "rotate("+ rotationValue + "deg)";
         //shape.style.mozTransform = "rotate("+ rotationValue + "deg)";
@@ -52,19 +54,35 @@
         
         if(y < wHeight/numOfSlides){
             container.style.transform = "translate(0px, 0px)";
+            currentSlide = 1;
         }
         if((y >= wHeight/numOfSlides) && (y < wHeight/numOfSlides * 2)){
             container.style.transform = "translate(0px, -100vh)";
+            currentSlide = 2;
         }
         if((y >= wHeight/numOfSlides * 2) && (y < wHeight/numOfSlides * 3)){
             container.style.transform = "translate(0px, -200vh)";
+            currentSlide = 3;
         }
         if((y >= wHeight/numOfSlides * 3) && (y < wHeight/numOfSlides * 4)){
             container.style.transform = "translate(0px, -300vh)";
+            currentSlide = 4;
         }
         if(y >= wHeight/numOfSlides * 4){
             container.style.transform = "translate(0px, -400vh)";
+            currentSlide = 5;
         }
+        markerActive(currentSlide);
+    }
+
+    function markerActive(s){
+        console.log(dot.length);
+        for (i = 0; i < dotContain.children.length; i++) {
+            dotContain.children[i].classList.remove("dotActive");
+            console.log("class removed");
+          }
+
+        dotContain.children[s * 2 - 2].classList.add("dotActive");
     }
 
 };
